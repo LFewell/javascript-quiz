d39ec1d12caf2922182c18cc6d4f2da7a00bdabc
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-21 18:41:38 -0400

    started an array of objects for the questions and made a text modifier for h1 and removed quiz.html

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var startButton = document.querySelector(".start-button")
+var h1 = document.getElementById("coding");
 
 // var time = 75;
 // var score = 0
@@ -28,7 +29,7 @@ startButton.addEventListener("click", function() {
         console.log("timer")
         var time = 75;
         var timer = setInterval(function() {
-            document.querySelector(".timer").innerHTML= time;
+            document.querySelector(".timer").innerHTML= "Time Left: " + time;
             time--;
             if (time < 0) {
                 clearInterval(timer);
@@ -36,3 +37,25 @@ startButton.addEventListener("click", function() {
         }, 1000)
 
 });
+
+startButton.addEventListener("click", buttonClick, false);
+
+function buttonClick() {
+    
+    h1.textContent = "You clicked the button";
+
+
+
+}
+
+var question = [
+    {
+        question1 : "Commonly used data",
+        choices1 : ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer1 : 2,
+    },
+
+    {
+        question2 : "The condition"
+    }
+]
