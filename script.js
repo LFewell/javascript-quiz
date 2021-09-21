2b04408f75321ebc057fa1d1bc628698d20624b5
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-21 12:36:23 -0400

    I NEVER LINKED TO MY JS LOL; timer is working

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 var startButton = document.querySelector(".start-button")
 
-var time = 75;
-var score = 0
+// var time = 75;
+// var score = 0
 
 
-function startGame() {
-    time = 75;
-    timer();
-    //maybe call render questions function here
-}
+// function startGame() {
+//     time = 75;
+//     timer();
+//     //maybe call render questions function here
+// }
 
-function timer() {
-    time--;
-    if (time <= 0) {
-        clearInterval(timer)
-    }
-}
+// function startTimer() {
+//     timer = setInterval(timer, 1000);
+//     timer();
+// }
 
+// function timer() {
+//     time--;
+//     if (time <= 0) {
+//         clearInterval(timer)
+//     }
+// }
 
+// document.getElementById("timer").innerHTML = time
 
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", function() {
+        console.log("timer")
+        var time = 75;
+        var timer = setInterval(function() {
+            document.querySelector(".timer").innerHTML= time;
+            time--;
+            if (time < 0) {
+                clearInterval(timer);
+            }
+        }, 1000)
+
+});
