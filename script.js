b8ad690dc7cb9ba7c3d4ffff6eeb6fa4b447c77c
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-09-22 14:21:26 -0400

    Changed the show/hide function for the initial page

--- script.js
+++ script.js
@@ -4,30 +4,6 @@ var displayQ = document.getElementById("question");
 var quizBox = document.getElementById("quiz-box");
 var questionBox = document.getElementById("question-box");
 
-// var time = 75;
-// var score = 0
-
-
-// function startGame() {
-//     time = 75;
-//     timer();
-//     //maybe call render questions function here
-// }
-
-// function startTimer() {
-//     timer = setInterval(timer, 1000);
-//     timer();
-// }
-
-// function timer() {
-//     time--;
-//     if (time <= 0) {
-//         clearInterval(timer)
-//     }
-// }
-
-// document.getElementById("timer").innerHTML = time
-
 startButton.addEventListener("click", function() {
         console.log("timer")
         var time = 75;
@@ -41,29 +17,28 @@ startButton.addEventListener("click", function() {
 
 });
 
-questionBox.style.display = "none";
 
-startButton.addEventListener("click", buttonClick);
+startButton.addEventListener("click", startQuiz);
 
-function buttonClick() {
-    if (questionBox.style.display = "block") {
-        quizBox.style.display = "none";
+function startQuiz() {
+    var intro = document.querySelector(".quiz-wrapper");
+    var initQuestion = document.querySelector(".question-wrapper");
 
-    } else {
-        questionBox.style.display = "none";
-        quizBox.style.display = "block";
-    }
+    intro.classList.toggle("is-hidden");
+    initQuestion.classList.toggle("is-hidden");
     displayQ.innerHTML = question[0].question1;
 }
 
+
+
 var question = [
     {
-        question1 : "Commonly used data",
+        question1 : "Commonly used data types DO NOT include:",
         choices1 : ["Strings", "Booleans", "Alerts", "Numbers"],
         answer1 : 2,
     },
 
     {
-        question2 : "The condition"
+        question2 : "The condition",
     }
 ]
